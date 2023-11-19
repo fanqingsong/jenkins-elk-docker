fddef61a066b39bd37f787a75fe2f4bee92c64e8
qingsong <qsfan@qq.com>
2023-11-19 21:48:12 +0800

    update

--- download.sh
+++ download.sh
@@ -3,13 +3,6 @@
 #clean anything with same name to get rid of clashes
 docker-compose down
 
-docker pull jenkins:2.60.0
-docker pull sonarqube:6.3.1
-docker pull docker.bintray.io/jfrog/artifactory-oss:5.4.4
-docker pull logstash:2
-docker pull elasticsearch:2
-docker pull kibana:4
-
 if [ ! -d downloads ]; then
     mkdir downloads
 #    curl -o downloads/jdk-8u131-linux-x64.tar.gz http://ftp.osuosl.org/pub/funtoo/distfiles/oracle-java/jdk-8u131-linux-x64.tar.gz
