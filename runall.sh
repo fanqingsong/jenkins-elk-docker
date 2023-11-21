61a8101054422a6113f10d521c2a5eb4d0304d2d
qingsong <qsfan@qq.com>
2023-11-21 18:05:19 +0800

    refactor

--- runall.sh
+++ runall.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-#get needed stuff 1st
-./download.sh
-
 #clean anything with same name to get rid of clashes
 docker-compose down
-
-#update with actual password
-echo "password" > ./secrets/artifactoryPassword
 
 #update older jenkins image, make sure it doesnt use cache
 docker-compose build --no-cache
